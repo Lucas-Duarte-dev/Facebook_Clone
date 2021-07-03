e33df0c064cfe97a4ef1b13acd0f5dabe0896acf
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-07-03 18:38:22 -0300

    Facebook clone... Ready? Go

---
+++ src/pages/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./style.module.scss";
+
+export function Header() {
+  return (
+    <div>
+      <header>
+        <img src="" alt="Logo" />
+        <input type="text" placeholder="Pesquisar no Facebook" />
+      </header>
+      <nav></nav>
+      <div></div>
+    </div>
+  );
+}
