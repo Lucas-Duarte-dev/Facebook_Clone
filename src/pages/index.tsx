d98a31e004fefae1618366c0ae71a825561dd144
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-07-06 19:02:44 -0300

    Created layout for add story

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from "../styles/homepage.module.scss";
+
 export default function Home() {
-  return <h1>hello World</h1>;
+  return (
+    <div className={styles.container}>
+      <section className={styles.storysContainer}>
+        <div>
+          <img
+            src="https://avatars.githubusercontent.com/u/66844060?v=4"
+            alt="User Story"
+          />
+          <div>
+            <img src="/icons/plus.svg" alt="Add" />
+          </div>
+          <span>Criar story</span>
+        </div>
+      </section>
+      <div className={styles.createPost}></div>
+    </div>
+  );
 }
