4323d060efe4a6dbdbad68c4fe7fc900aa6306e0
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-07-12 20:40:20 -0300

    Custom page not found

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/error.module.scss";
+
+export default function Custom404() {
+  return (
+    <div className={styles.error}>
+      <h1>404 - Página não encontrada</h1>
+    </div>
+  );
+}
